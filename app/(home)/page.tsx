73e08a645d2a9844e53314aab110459abd6d8490
DiulioAires <diulioaires@outlook.com>
2026-03-21 18:58:57 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -36,7 +36,22 @@ export default async function Home() {
         <h2 className="text-xs uppercase px-5 text-gray-400 font-bold mb-3">Recomendados</h2>
         
 
-        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
+        <div className="flex px-5gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
+           {barbershops.map((barbershop) => (
+          <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+
+           ))}
+        </div>
+
+
+      </div>
+
+      
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="text-xs uppercase px-5 text-gray-400 font-bold mb-3">Populares</h2>
+        
+
+        <div className="flex px-5gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden  epx-5">
            {barbershops.map((barbershop) => (
           <BarberShopItem key={barbershop.id} barbershop={barbershop} />
 
