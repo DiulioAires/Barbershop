c4120e0822b78d16663689169b5575dfd5bfc383
DiulioAires <diulioaires@outlook.com>
2026-03-27 23:35:05 -0300

    fix: add postinstall

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -11,13 +11,13 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
     const connectionString = process.env.DATABASE_URL;
     const pool = new Pool({ connectionString }); // Ensure this is the same version as used by @prisma/adapter-pg
-    const adapter = new PrismaPg(pool);
+    const adapter = new PrismaPg(pool as any);
     prisma = new PrismaClient({ adapter });
 } else {
     if (!global.cachedPrisma) {
         const connectionString = process.env.DATABASE_URL;
         const pool = new Pool({ connectionString });
-        const adapter = new PrismaPg(pool);
+        const adapter = new PrismaPg(pool as any);
         global.cachedPrisma = new PrismaClient({ adapter });
     }
     prisma = global.cachedPrisma;
